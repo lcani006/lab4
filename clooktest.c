e933cbae9bfdb1d02f9320217c7e779adb4ac5ab
lcani006 <lcani006@fiu.edu>
2019-11-29 17:38:12 -0500

    added clooktest

---
+++ clooktest.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+   
+   
+   printf("Reading and Writing to iotest.txt");
+   
+   
+   FILE *file;
+   file = (fopen("iotest.txt", "a"));
+   if(file == NULL)
+   {
+       printf("Error!");
+       exit(1);
+   }
+
+    int i = 0;
+    int num = 10;
+    char buffer[256];
+   for(i = 0; i < num; ++i)
+   {
+      
+      fprintf(file,"Test %d \n", i + 1);
+      fgets(buffer, 10, file);
+   }
+
+   fclose(file);
+   return 0;
+}
